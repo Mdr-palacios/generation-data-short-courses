ff1646cec1e58a63418a32534f9830e2c3a2346f
Mdr-palacios <mdr@generationdata.org>
2026-08-07 15:49:07 +0000

    Connect signup form to live Apps Script Web App endpoint

--- assets/config.js
+++ assets/config.js
@@ -3,5 +3,5 @@
 // After you deploy the Apps Script Web App (see /apps-script/README.md),
 // paste the deployment URL below between the quotes. It ends in /exec.
 window.SIGNUP_CONFIG = {
-  appsScriptUrl: ""
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwEgfzanXNg2nEaDVr5kW_YwxrDMmXJXXn3iQZB78tYAQt56uh3V8dI5S2ltPna106u/exec"
 };
